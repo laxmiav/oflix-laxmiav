05de128361db8c55a83131d1933bb2867208257b
muthulakshmi-git <laxmiavmc@outlook.com>
2022-02-17 16:01:35 +0100

    homepage

--- sources/routes.md
+++ sources/routes.md
@@ -3,4 +3,6 @@
 | URL | Méthode HTTP | Contrôleur       | Méthode | Titre HTML           | Commentaire    |
 | --- | ------------ | ---------------- | ------- | -------------------- | -------------- |
 | `/` | `GET`        | `MainController` | `home`  | Bienvenue sur O'flix | Page d'accueil |
-
+| `/movie/{id}` | `GET`        | `MovieController` | `show`  | Détail du film { nom du film} | Page de détail d'un film |
+| `/favorites` | `GET`        | `MovieController` | `favorites`  | Vos favoris | Page des favoris |
+| `/list` | `GET`        | `MovieController` | `showList`  | Liste des films | Liste de films |
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,30 +1,24 @@
-<?php
+<?php 
 
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController {
 
-    public function homepage() 
-    {
-        $response = new Response('<h1>Hello Symfo</h1>');
 
-        // 1ere règle de Symfony : une action doit retourner un objet de la classe Symfony\Component\HttpFoundation\Response
-        return $response;
+    /**
+     * displays the homepage
+     *
+     * @return Response
+     * 
+     * @Route("/", name="homepage")
+     */
+    public function home() :Response
+    {
+        return $this->render('main/homepage.html.twig');
     }
 
-    public function testJson() 
-    {
-        $data = [
-            'coucou' => "c'est dla balle",
-            'nom' => 'grego',
-            'chevelu' => false,
-        ];
-
-        // $this->json renvoie un objet de la classe JsonResponse qui est enfant de la classe Response
-        // donc on respecte la 1ère règle de Symfony
-        return $this->json($data);
-    }
 }
