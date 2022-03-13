d7ca02187c968620aa9754ef88b1713067d92fb0
muthulakshmi-git <laxmiavmc@outlook.com>
2022-03-13 22:49:00 +0100

    change the <body> in dump

--- src/EventSubscriber/MaintenenceSubscriber.php
+++ src/EventSubscriber/MaintenenceSubscriber.php
@@ -4,19 +4,31 @@ namespace App\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
 
 class MaintenenceSubscriber implements EventSubscriberInterface
 {
     public function onKernelResponse(ResponseEvent $event)
     {
        $response = $event->getResponse();
-       dump($response);
+
+    $replace = str_replace('<body>','<body><div class="alert alert-danger">Maintenance prévue mardi 10 janvier à 17h00</div>',$response);
+     //$response->request->set('<body>', $replace);
+       //dump($replace);
     }
+
+
+
+
+
+
 
     public static function getSubscribedEvents()
     {
         return [
             'kernel.response' => 'onKernelResponse',
+            KernelEvents::RESPONSE => 'onKernelResponse',
         ];
     }
 }
